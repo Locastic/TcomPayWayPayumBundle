d2a14014505b037aa186161458d0d17fcb791da2
Kruno Knego <k.knego@gmail.com>
2015-01-29 12:04:38 +0100

    Sylius Bridge updated.

--- Bridge/Sylius/CapturePaymentAction.php
+++ Bridge/Sylius/CapturePaymentAction.php
@@ -15,127 +15,81 @@ use Payum\Core\Request\ObtainCreditCard;
 class CapturePaymentAction extends PaymentAwareAction
 {
     /**
-     * @var EngineInterface
+     * @var GenericTokenFactoryInterface
      */
-    protected $templating;
+    protected $tokenFactory;
 
     /**
-     * @var string
+     * @param GenericTokenFactoryInterface $tokenFactory
      */
-    protected $templateName;
-
-    /**
-     * @var Symfony\Component\HttpFoundation\Request
-     */
-    protected $request;
-
-    /**
-     * @param EngineInterface $templating
-     * @param string $templateName
-     * @param Request $request
-     */
-    public function __construct(EngineInterface $templating, $templateName, $request)
+    public function __construct(GenericTokenFactoryInterface $tokenFactory)
     {
-        $this->templating = $templating;
-        $this->templateName = $templateName;
-        $this->request = $request;
-
-        if ($this->request->get('PaRes') !== null) {
-            $_SESSION['PaRes'] = $this->request->get('PaRes');
-        }
+        $this->tokenFactory = $tokenFactory;
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
+     *
+     * @param $request Capture
      */
     public function execute($request)
     {
-        /** @var $request Capture */
-        if (false == $this->supports($request)) {
-            throw RequestNotSupportedException::createActionNotSupported($this, $request);
-        }
+        RequestNotSupportedException::assertSupports($this, $request);
 
         /** @var $payment PaymentInterface */
         $payment = $request->getModel();
 
-        /** @var OrderInterface $order */
-        $order = $payment->getOrder();
+        $this->composeDetails($payment, $request->getToken());
 
-        $details = $payment->getDetails();
-
-        if (empty($details)) {
-            $this->payment->execute($obtainCreditCardRequest = new ObtainCreditCard($order));
-            $creditCard = $obtainCreditCardRequest->obtain();
-
-            $details = array();
-            $details['firstName'] = $order->getBillingAddress()->getFirstName();
-            $details['lastName'] = $order->getBillingAddress()->getLastName();
-            $details['email'] = $order->getUser()->getEmail();
-            $details['street'] = $order->getBillingAddress()->getStreet();
-            $details['city'] = $order->getBillingAddress()->getCity();
-            $details['postalCode'] = $order->getBillingAddress()->getPostcode();
-            $details['country'] = $order->getBillingAddress()->getCountry()->getName();
-            $details['phoneNumber'] = ''; // ?: 'Unknown'
-            $details['amount'] = 2350; //$order->getTotal();
-
-//            $details['card'] = new SensitiveValue(array(
-//                'number'      => $creditCard->getNumber(),
-//                'expiryMonth' => $creditCard->getExpiryMonth(),
-//                'expiryYear'  => $creditCard->getExpiryYear(),
-//                'cvd'         => $creditCard->getSecurityCode()
-//            ));
-
-            $details['card_number'] = $creditCard->getNumber();
-            $details['card_expiryMonth'] = $creditCard->getExpiryMonth();
-            $details['card_expiryYear'] = $creditCard->getExpiryYear();
-            $details['card_cvd'] = $creditCard->getSecurityCode();
-
-            $details['numOfInstallments'] =  1;
-
-            $details['shoppingCartId'] =  $order->getNumber() . "-" . rand();
-            $details['paymentMode'] =  1;
-
-            $details['httpAccept'] = $this->request->headers->get('Accept');
-            $details['httpUserAgent'] = $this->request->headers->get('User-Agent');
-            $details['originIP'] = $this->request->getClientIp();
-
-            $payment->setDetails($details);
-        }
-
-        $details = ArrayObject::ensureArrayObject($details);
+        $details = ArrayObject::ensureArrayObject($payment->getDetails());
 
         try {
             $request->setModel($details);
             $this->payment->execute($request);
 
-            $payment->setDetails((array) $details);
+            $payment->setDetails($details);
             $request->setModel($payment);
-        } catch (HttpRedirect $interactiveRequest) {
-            $payment->setDetails((array) $details);
+        } catch (\Exception $e) {
+            $payment->setDetails($details);
             $request->setModel($payment);
-//            $rawDetails = (array) $details;
 
-            throw $interactiveRequest;
-
-//            throw new ResponseInteractiveRequest($this->templating->render(
-//                $this->templateName,
-//                array(
-//                    "ASCUrl" => $rawDetails['ASCUrl'],
-//                    "PaReq" => $rawDetails['PaReq'],
-//                    "TermUrl" => $rawDetails['PaReq']
-//                )
-//            ));
+            throw $e;
         }
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
+     */
+    protected function composeDetails(PaymentInterface $payment, TokenInterface $token)
+    {
+        if ($payment->getDetails()) {
+            return;
+        }
+
+        $order = $payment->getOrder();
+
+        $details = array();
+
+        $details['firstName'] = $order->getBillingAddress()->getFirstName();
+        $details['lastName'] = $order->getBillingAddress()->getLastName();
+        $details['email'] = $order->getUser()->getEmail();
+        $details['address'] = $order->getBillingAddress()->getAddress();
+        $details['city'] = $order->getBillingAddress()->getCity();
+        $details['zipCode'] = $order->getBillingAddress()->getZipCode();
+        $details['country'] = $order->getBillingAddress()->getCountry();
+        $details['phoneNumber'] = $order->getBillingAddress()->getPhoneNumber();
+
+        $payment->setDetails($details);
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function supports($request)
     {
         return
             $request instanceof Capture &&
             $request->getModel() instanceof PaymentInterface
-        ;
+            ;
     }
 }
