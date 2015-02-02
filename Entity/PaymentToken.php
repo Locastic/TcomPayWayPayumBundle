a2d53477983449c947526a8290dcadc3cbb0737c
Kruno Knego <k.knego@gmail.com>
2015-02-02 13:37:51 +0100

    PaymentToken updated.

--- Entity/PaymentToken.php
+++ Entity/PaymentToken.php
@@ -10,10 +10,4 @@ use Payum\Core\Model\Token;
  */
 class PaymentToken extends Token
 {
-    /**
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    protected $id;
 }
