03885348cee3ab86f3ae0f704a6d189e58f2ea45
Antonio Peric <antonio@locastic.com>
2015-07-23 17:26:07 +0200

    added test for fill order details action

---
+++ Tests/Action/CaptureOffSiteActionTest.php
@@ -0,0 +1,89 @@
+<?php
+namespace Locastic\TcomPayWayPayumBundle\Tests\Action;
+
+use Locastic\TcomPayWayPayumBundle\Action\FillOrderDetailsAction;
+use Payum\Core\Model\Order;
+use Payum\Core\Request\FillOrderDetails;
+use Payum\Core\Tests\GenericActionTest;
+
+class FillOrderDetailsActionTest extends GenericActionTest
+{
+    protected $actionClass = 'Locastic\TcomPayWayPayumBundle\Action\FillOrderDetailsAction';
+
+    protected $requestClass = 'Payum\Core\Request\FillOrderDetails';
+
+    public function provideSupportedRequests()
+    {
+        return array(
+            array(new $this->requestClass(new Order())),
+            array(new $this->requestClass($this->getMock('Payum\Core\Model\OrderInterface'))),
+            array(new $this->requestClass(new Order(), $this->getMock('Payum\Core\Security\TokenInterface'))),
+        );
+    }
+
+    public function provideNotSupportedRequests()
+    {
+        return array(
+            array('foo'),
+            array(array('foo')),
+            array(new \stdClass()),
+            array($this->getMockForAbstractClass('Payum\Core\Request\Generic', array(array()))),
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCorrectlyConvertOrderToDetailsAndSetItBack()
+    {
+        $order = new Order();
+        $order->setNumber('theNumber');
+        $order->setCurrencyCode('USD');
+        $order->setTotalAmount(123);
+        $order->setDescription('the description');
+        $order->setClientId('theClientId');
+        $order->setClientEmail('theClientEmail');
+
+        $action = new FillOrderDetailsAction();
+
+        $action->execute(new FillOrderDetails($order));
+
+        $details = $order->getDetails();
+
+        $this->assertNotEmpty($details);
+
+        $this->assertArrayHasKey('pgwAmount', $details);
+        $this->assertEquals(123, $details['pgwAmount']);
+
+        $this->assertArrayHasKey('pgwOrderId', $details);
+        $this->assertEquals('theNumber', $details['pgwOrderId']);
+
+        $this->assertArrayHasKey('pgwEmail', $details);
+        $this->assertEquals('theClientEmail', $details['pgwEmail']);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotOverwriteAlreadySetExtraDetails()
+    {
+        $order = new Order();
+        $order->setCurrencyCode('USD');
+        $order->setTotalAmount(123);
+        $order->setDescription('the description');
+        $order->setDetails(array(
+            'foo' => 'fooVal',
+        ));
+
+        $action = new FillOrderDetailsAction();
+
+        $action->execute(new FillOrderDetails($order));
+
+        $details = $order->getDetails();
+
+        $this->assertNotEmpty($details);
+
+        $this->assertArrayHasKey('foo', $details);
+        $this->assertEquals('fooVal', $details['foo']);
+    }
+}
